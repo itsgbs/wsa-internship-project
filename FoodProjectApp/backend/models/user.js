aabe83dfaa15850fc3f882f9e191d40a71488bbe
Govind_Ballab_Singhania <govindsinghania188@gmail.com>
2026-06-09 20:37:05 +0530

    modified:   FoodProjectApp/backend/models/user.js

--- FoodProjectApp/backend/models/user.js
+++ FoodProjectApp/backend/models/user.js
@@ -1,24 +1,11 @@
 //Schema or Structure of User Database
 
-//REQUIRED PACKAGES. (to run in terminal)
 
-//1. crypto is a built in node js module that is used to generate secure random values like hashes or tokens
-// we use crypto to generate tokens for password authentication, email verification, forgot password
-// reset token  when user forgot password
+// Installation Command
+// npm i jsonwebtoken bcryptjs nodemailer validator html-to-text cloudinary
+// Note:
 
-// 2. jsonwebtoken is used to identify whether user is logged in or not. SO That user don't have to login every time 
-// user carries that token with every request to access website. that token will expire depending on settings of website for jwt
-// at time of login, protected routes, user sessions. it creates login authentication token
-
-//3. bcryptjs is a packages used to hash password securely. never store password of user in form of normal password in database.
-//so we hash or encyrpt it to prevent attacks from hackers
-
-//4. nodemailer is used to send emails from node js application. so important notifications will come in email.
-
-//5. validator used to check format of email. users can enter anything so we check whether it is valid in email field or not
-
-//6. html-to-text = converts html content into plain text. ensures email compatibility across all devices.
-//7. cloudinary -  cloud based service for storing images and videos
+// crypto does NOT need to be installed because it is a built-in Node.js module.
 
 //STEP 1 - IMPORT
 const mongoose = require("mongoose")
